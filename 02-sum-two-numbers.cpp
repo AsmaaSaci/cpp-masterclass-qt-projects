618846532e11d435a9b7b7c0c14335dce235285e
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-02 20:32:15 +0100

    Create 02-sum-two-numbers.cpp

---
+++ 02-sum-two-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+int main() {
+    int x , y ;
+    int sum = 0;
+    cout << "enter your first number" << endl; 
+    cin >> x;
+    cout << "enter your secend number" << endl; 
+    cin >> y; 
+    sum = x+y;
+
+    cout << "the sam is = "<< sum;
+
+    return 0;
+}
