931f153f471ad91300eddac69bdefae180e464d1
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-03 11:38:46 +0100

    Create 18-assignment-4.cpp

---
+++ 18-assignment-4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0;
+    int i = 1;
+
+    while (i <= 10) {
+        sum += i;
+        i++;
+    }
+
+    cout << "The sum of the first 10 natural numbers is: " << sum << endl;
+
+    return 0;
+}
