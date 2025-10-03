df4dc51c37fe52e0cec9f999871b4b2a80d009ce
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-03 11:03:30 +0100

    Create 11-multiple-of-3.cpp

---
+++ 11-multiple-of-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int number=0;
+   cout << " enter a number "<< endl;
+   cin >> number ;
+   //cal
+   if (number %3==0)
+       cout << " your number is multiple of 3 "<< endl ;
+   else 
+       cout << " your number is not a multiple of 3  "<< endl ;
+   
+    
+    return 0;
+}
