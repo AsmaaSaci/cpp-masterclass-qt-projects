2a3782cf1ac0eebf74f62913a6d684aca44bebfb
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-03 10:59:37 +0100

    Create 10-else-if-condition.cpp

---
+++ 10-else-if-condition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int number=0;
+   cout << " enter a number "<< endl;
+   cin >> number ;
+   //cal
+   if (number %2==0)
+       cout << " your number is even "<< endl ;
+   else 
+       cout << " your number is odd  "<< endl ;
+   
+    
+    return 0;
+}
