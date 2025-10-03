f25dd42fa2dddc72d1e782656531f696ebf37f5d
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-03 23:17:50 +0100

    Create 23-array-minimum.cpp

---
+++ 23-array-minimum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int arr [5]={0};
+    for (int i=0 ;i<5; i++)
+    {
+    cout << endl << "enter number "<< i<< ":";
+    cin >> arr[i];
+    }
+    int min = arr [0];
+    for (int i =1;i<5;i++){
+        if (min > arr[i])
+        {min = arr[i];
+            
+        }
+    }
+    cout << endl << " min value is :"<< min ;
+    return 0;
+}
